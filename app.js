322420940daf96d2bd1d3119f7473ef2ff554cd1
Fazle Bari <fazlebarisn@gmail.com>
2021-06-11 15:55:28 +0600

    add the remove function when someone click Remove button

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log(1111111);
+var removeCartButtons = document.getElementsByClassName("btn-danger");
+
+for( i=0; i<removeCartButtons.length; i++ ){
+    var button = removeCartButtons[i];
+    button.addEventListener("click" , function(event){
+        var buttonClicked = event.target;
+        buttonClicked.parentElement.parentElement.remove()
+    })
+}
